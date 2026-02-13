bf47befac41acc96da20dc90a6b6a4ea0e370629
BharathJP-72 <bharathjp72@gmail.com>
2026-02-14 00:14:56 +0530

    Revert "JS!"
    This reverts commit e2649aa3a8d4ac8893d9d27d781f4e733342db62.

--- markX.js
+++ markX.js
@@ -1,8 +1,6 @@
 console.log("Hello, Mark X!");
 console.log("Welcome to the world of JavaScript programming.");
 
-
-console.log("Let's explore some new features in JavaScript together!");
 console.log('REVERTTTTTTTT');
 
  

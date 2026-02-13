f3ceb45325df43095eb64e3d793ef8a206110b03
BharathJP-72 <bharathjp72@gmail.com>
2026-02-14 00:26:55 +0530

    Learnt stash

--- markX.js
+++ markX.js
@@ -4,3 +4,7 @@ console.log("Welcome to the world of JavaScript programming.");
 console.log('REVERTTTTTTTT');
 
  // This is a simple JavaScript file that prints messages to the console. It includes a greeting, a welcome message, and a note about reverting changes.
+//hi
+//code
+//code
+//code
